cae55d5febbe4c5676ddcec9e557560ac06bb559
Amaury Diaz <amauryuh@gmail.com>
2023-05-27 21:55:23 +0200

    release v0.1.4

--- domain/booking/v1/slot.pb.validate.go
+++ domain/booking/v1/slot.pb.validate.go
@@ -95,6 +95,35 @@ func (m *Slot) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetAvailability()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, SlotValidationError{
+					field:  "Availability",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, SlotValidationError{
+					field:  "Availability",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetAvailability()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return SlotValidationError{
+				field:  "Availability",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return SlotMultiError(errors)
 	}
